6ffc89c432dcf52bba9157fa6fc024bc227e0b5e
Parzival <he17637518479@icloud.com>
2021-08-04 21:42:51 +0800

    first commit

---
+++ src/main/java/com/light/gitdemo/controller.java
@@ -0,0 +1,12 @@
+package com.light.gitdemo;
+
+/**
+ * @author light
+ * @create 2021-08-04 9:41 下午
+ */
+
+public class controller {
+    public static void main(String[] args) {
+        System.out.println("version1");
+    }
+}
